22590cd207f77ab8dfa55aafaabcaa5f075e5e54
Stas Raranetskyi <rar.stas@gmail.com>
2019-05-19 14:48:38 +0300

    React native cli sqlite (#2)
    * test
    
    * react native cli. create
    
    * connect
    
    * fix database opening
    
    * class bd

--- App.js
+++ App.js
@@ -1,21 +1,60 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import Db from './src/db';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+
+    constructor() {
+        super();
+    }
+
+    async componentDidMount(){
+        const res = await Db.execute('select * from list');
+        console.log('res!!!!!!!! =>>>>>>>>', res);
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>Welcome to React Native!!</Text>
+                <Text style={styles.instructions}>To get started, edit App.js</Text>
+                <Text style={styles.instructions}>{instructions}</Text>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
   },
 });
